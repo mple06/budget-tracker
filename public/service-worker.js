f7334edea5c3fda76e3c86f6d55226d99ca49714
Minhvan Le <mple330@gmail.com>
2021-11-18 20:20:26 -0800

    added downloadable icon on top

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,10 @@
 const FILES_TO_CACHE = [
     "/",
+    "/db.js",
     "/index.html",
     "/index.js",
     "/styles.css",
+    "/manifest.webmanifest",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
 ];
